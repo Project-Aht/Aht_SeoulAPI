b5c2d4dcc233a8c404910fad0f5ba9068bff1a80
dodo07070707 <aa01056328583@gmail.com>
2023-09-21 21:56:33 +0900

    grade calculator screen add

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:firebase_core/firebase_core.dart';
-
 import 'firebase_options.dart';
 import 'firebase/instance.dart';
 import 'screens/register_exam_screen.dart';
+import 'screens/grade_calculator_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -26,9 +26,14 @@ class MainApp extends StatelessWidget {
             children: [
               ElevatedButton(
                   onPressed: () {
-                    () => const ResgisterExamScreen();
+                    Get.to(() => const ResgisterExamScreen());
                   },
                   child: const Text('정보 추가하기')),
+              ElevatedButton(
+                  onPressed: () {
+                    Get.to(() => const GradeCalclulatorScreen());
+                  },
+                  child: const Text('성적 계산기')),
             ],
           ),
         ),
---
+++ lib/screens/grade_calculator_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GradeCalclulatorScreen extends StatefulWidget {
+  const GradeCalclulatorScreen({super.key});
+
+  @override
+  State<GradeCalclulatorScreen> createState() => _GradeCalclulatorScreenState();
+}
+
+class _GradeCalclulatorScreenState extends State<GradeCalclulatorScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
