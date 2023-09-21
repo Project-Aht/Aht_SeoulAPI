e436062e03fbdfb55c4ccaf0742be242712e02dd
dodo07070707 <aa01056328583@gmail.com>
2023-09-21 22:02:27 +0900

    color theme edit

--- lib/themes/color_theme.dart
+++ lib/themes/color_theme.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
 abstract class AhtColors {
-  static const Main_Color = Color(0xFF00B375);
-  static const Regi_Hint_Color = Color(0xFFD9D9D9);
+  static const Main_Color = Color(0xFF38498E);
 }
