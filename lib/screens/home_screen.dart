efb3a1130b1fdcf50d184ab36de5272606f2a1a8
dodo07070707 <aa01056328583@gmail.com>
2023-10-12 21:03:24 +0900

    home screen building

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,7 +3,9 @@ import 'package:get/get.dart';
 import 'package:aht_dimigo/themes/color_theme.dart';
 import 'package:aht_dimigo/screens/register_exam_screen.dart';
 import 'package:aht_dimigo/screens/grade_calculator_screen.dart';
+import '../widgets/custom_text.dart';
 import 'myinfo_screen.dart';
+import 'package:aht_dimigo/widgets/subject_selection_box.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -17,6 +19,8 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     double screenHeight = MediaQuery.of(context).size.height;
     double screenWidth = MediaQuery.of(context).size.width;
+    Set<String> subjectSet = {'수학', '과학', '국어', '영어', '프밍'};
+    List<String> subjectList = subjectSet.toList();
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
@@ -64,8 +68,8 @@ class _HomeScreenState extends State<HomeScreen> {
                                 ),
                               ),
                               SizedBox(width: screenWidth / 390 * 14),
-                              const Text(
-                                '한국디지털미디어고 \n1학년 2반 22번',
+                              const CustomText(
+                                text: '한국디지털미디어고 \n1학년 2반 22번',
                                 style: TextStyle(
                                   color: Colors.white,
                                   fontSize: 16,
@@ -96,23 +100,78 @@ class _HomeScreenState extends State<HomeScreen> {
                 ],
               ),
             ),
+            SizedBox(height: screenHeight / 844 * 16),
+            Row(
+              children: [
+                SizedBox(width: screenWidth / 390 * 32),
+                SizedBox(
+                  height: screenHeight / 844 * 31,
+                  child: Row(
+                    children: [
+                      SizedBox(
+                        width: screenWidth / 390 * 259,
+                        child: ListView.separated(
+                          scrollDirection: Axis.horizontal,
+                          shrinkWrap: true,
+                          itemCount: subjectList.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return SubjectSelectionBox(subjectList[index]);
+                          },
+                          separatorBuilder: (BuildContext context, int index) =>
+                              SizedBox(
+                            width: screenWidth / 390 * 10,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(width: screenWidth / 390 * 10),
+                Container(
+                  width: screenWidth / 390 * 57,
+                  height: screenHeight / 844 * 31,
+                  decoration: ShapeDecoration(
+                    color: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      side: const BorderSide(
+                          width: 0.50, color: Color(0xFFC4C4C4)),
+                      borderRadius: BorderRadius.circular(19),
+                    ),
+                  ),
+                  child: const Center(
+                    child: CustomText(
+                      text: '+',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontFamily: 'Pretendard',
+                        fontWeight: FontWeight.w100,
+                        height: 1.25,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: screenWidth / 390 * 32),
+              ],
+            ),
             ElevatedButton(
               onPressed: () {
                 Get.to(() => const ResgisterExamScreen());
               },
-              child: const Text('정보 추가하기'),
+              child: const CustomText(text: '정보 추가하기'),
             ),
             ElevatedButton(
               onPressed: () {
                 Get.to(() => const GradeCalclulatorScreen());
               },
-              child: const Text('성적 계산기'),
+              child: const CustomText(text: '성적 계산기'),
             ),
             ElevatedButton(
               onPressed: () {
                 Get.to(() => const MyinfoScreen());
               },
-              child: const Text('내 정보'),
+              child: const CustomText(text: '내 정보'),
             ),
           ],
         ),
