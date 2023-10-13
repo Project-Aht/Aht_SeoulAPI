cb3580ff476dac1cb95233eaf91e867d00ad3798
diw28 <diw28@dimigo.hs.kr>
2023-10-13 09:30:20 +0900

    texttheme

--- lib/themes/text_theme.dart
+++ lib/themes/text_theme.dart
@@ -8,4 +8,18 @@ abstract class AhtTextTheme {
     fontWeight: FontWeight.w700,
     height: 1.6,
   );
+  static const SubjectCardHighlight = TextStyle(
+    color: Colors.black,
+    fontSize: 16,
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w400,
+    height: 1.25,
+  );
+  static const SubjectCardDetail = TextStyle(
+    color: Colors.white60,
+    fontSize: 16,
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w400,
+    height: 1.25,
+  );
 }
